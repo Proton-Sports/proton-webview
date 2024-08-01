b181fba7f8e6136a92ddebf4dc5e41c6a354e476
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-08-01 18:26:42 +0700

    feat: toggle scoreboard using X key

--- src/routes/race-finish-scoreboard/Index.tsx
+++ src/routes/race-finish-scoreboard/Index.tsx
@@ -1,9 +1,9 @@
 import clsx from 'clsx';
-import styles from './styles.module.css';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
-import { formatRelativeTimeParts } from '../../lib/utils/format';
 import Marquee from 'react-fast-marquee';
+import { formatRelativeTimeParts } from '../../lib/utils/format';
+import styles from './styles.module.css';
 
 const flags = [
   'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fechesters.co.uk%2Fimages%2Fposts%2Fflag-of-spain.png&f=1&nofb=1&ipt=75e5c8ac77aa46c67ef0c9782e2c83812c51a1326dd7b589052a34b7650b2300&ipo=images',
@@ -28,97 +28,113 @@ interface Data {
 
 export default function Index() {
   const [data, setData] = useState<Data | null>(null);
+  const [visible, setVisible] = useState(true);
 
   useEffect(() => {
     function handleGetData(data: Data) {
       setData(data);
     }
 
+    function handleToggle() {
+      setVisible((v) => !v);
+    }
+
     alt.emit('race-finish:getData');
     alt.on('race-finish:getData', handleGetData);
+    alt.on('race-finish-scoreboard:toggle', handleToggle);
     return () => {
       alt.off('race-finish:getData', handleGetData);
     };
   }, []);
 
-  if (!data) {
-    return null;
-  }
-
   return (
-    <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 space-y-8 fugaz">
-      <div className="flex items-stretch text-2xl rounded-md uppercase">
-        <div className="relative">
-          <div className="absolute inset-0 bg-bg/90" />
-          <div className="bg-primary pl-4 pr-12 py-2 -mr-2 fugaz [clip-path:polygon(0_0,_100%_0,_75%_100%,_0%_100%)]">
-            Results
-          </div>
-        </div>
-        <div className="flex grow items-center justify-center text-nowrap bg-bg/90 pr-4 text-xl">{data.name}</div>
-      </div>
-      <div className="max-h-[36rem] min-h-[36rem] overflow-y-auto overflow-x-visible bg-bg-1/90">
-        <table className="w-full table-auto text-left min-w-[56rem]">
-          <thead className="uppercase">
-            <motion.tr
-              className="*:px-2 *:py-1 bg-primary backdrop-blur sticky top-0 z-50"
-              initial={{ opacity: 0, x: '-4rem' }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.4, ease: 'easeInOut' }}
-            >
-              <th className="border-none">Pos.</th>
-              <th>Driver</th>
-              <th>Team</th>
-              <th>Time</th>
-            </motion.tr>
-          </thead>
-          <tbody className="overflow-y-scroll">
-            {data.participants.map(({ name, team, timeMs }, i) => {
-              const parts = formatRelativeTimeParts(timeMs);
-              return (
-                <motion.tr
-                  className="*:px-2 *:py-1 *:even:bg-bg-3/30"
-                  initial={{ opacity: 0, x: '-4rem' }}
-                  animate={{ opacity: 1, x: 0 }}
-                  transition={{ duration: 0.4, delay: i * 0.05, ease: 'easeInOut' }}
-                >
-                  <td>{i + 1}.</td>
-                  <td className="flex items-center gap-2">
-                    <img
-                      src={flags[Math.floor(Math.random() * flags.length)]}
-                      className="w-auto max-h-full max-w-4 aspect-[1/2] object-cover object-center -my-1"
-                    />
-                    {name}
-                  </td>
-                  <td>{team}</td>
-                  <td>
-                    <span>
-                      {parts[1]}:{parts[2]}.{parts[3]}
-                    </span>
-                  </td>
-                </motion.tr>
-              );
-            })}
-          </tbody>
-          <tfoot>
-            <motion.tr
-              className="bg-primary absolute flex bottom-0 w-full backdrop-blur *:px-2 *:py-1"
-              initial={{ opacity: 0, y: '100%' }}
-              animate={{ y: 0, opacity: 1 }}
-              transition={{ duration: 0.4, delay: 0.05 * 13 + 0.4, ease: 'easeInOut' }}
-            >
-              <td colSpan={4} className={clsx('w-full')}>
-                <div className={styles.wrapper}>
-                  <Marquee autoFill>
-                    {logos.map((logo) => (
-                      <img src={logo} className="w-24 h-auto object-cover px-4" />
-                    ))}
-                  </Marquee>
+    <AnimatePresence>
+      {data && visible && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.2, ease: 'easeInOut' }}
+        >
+          <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 space-y-8 fugaz">
+            <div className="text-right text-sm">
+              Press <span className="px-2 py-0.5 bg-bg-2 rounded">X</span> to show/hide
+            </div>
+            <div className="flex items-stretch text-2xl rounded-md uppercase !mt-2">
+              <div className="relative">
+                <div className="absolute inset-0 bg-bg/90" />
+                <div className="bg-primary pl-4 pr-12 py-2 -mr-2 fugaz [clip-path:polygon(0_0,_100%_0,_75%_100%,_0%_100%)]">
+                  Results
                 </div>
-              </td>
-            </motion.tr>
-          </tfoot>
-        </table>
-      </div>
-    </div>
+              </div>
+              <div className="flex grow items-center justify-center text-nowrap bg-bg/90 pr-4 text-xl">{data.name}</div>
+            </div>
+            <div className="max-h-[36rem] min-h-[36rem] overflow-y-auto overflow-x-visible bg-bg-1/90">
+              <table className="w-full table-auto text-left min-w-[56rem]">
+                <thead className="uppercase">
+                  <motion.tr
+                    className="*:px-2 *:py-1 bg-primary backdrop-blur sticky top-0 z-50"
+                    initial={{ opacity: 0, x: '-4rem' }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.4, ease: 'easeInOut' }}
+                  >
+                    <th className="border-none">Pos.</th>
+                    <th>Driver</th>
+                    <th>Team</th>
+                    <th>Time</th>
+                  </motion.tr>
+                </thead>
+                <tbody className="overflow-y-scroll">
+                  {data.participants.map(({ name, team, timeMs }, i) => {
+                    const parts = formatRelativeTimeParts(timeMs);
+                    return (
+                      <motion.tr
+                        className="*:px-2 *:py-1 *:even:bg-bg-3/30"
+                        initial={{ opacity: 0, x: '-4rem' }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.4, delay: i * 0.05, ease: 'easeInOut' }}
+                      >
+                        <td>{i + 1}.</td>
+                        <td className="flex items-center gap-2">
+                          <img
+                            src={flags[Math.floor(Math.random() * flags.length)]}
+                            className="w-auto max-h-full max-w-4 aspect-[1/2] object-cover object-center -my-1"
+                          />
+                          {name}
+                        </td>
+                        <td>{team}</td>
+                        <td>
+                          <span>
+                            {parts[1]}:{parts[2]}.{parts[3]}
+                          </span>
+                        </td>
+                      </motion.tr>
+                    );
+                  })}
+                </tbody>
+                <tfoot>
+                  <motion.tr
+                    className="bg-primary absolute flex bottom-0 w-full backdrop-blur *:px-2 *:py-1"
+                    initial={{ opacity: 0, y: '100%' }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 0.4, delay: 0.05 * 13 + 0.4, ease: 'easeInOut' }}
+                  >
+                    <td colSpan={4} className={clsx('w-full')}>
+                      <div className={styles.wrapper}>
+                        <Marquee autoFill>
+                          {logos.map((logo) => (
+                            <img src={logo} className="w-24 h-auto object-cover px-4" />
+                          ))}
+                        </Marquee>
+                      </div>
+                    </td>
+                  </motion.tr>
+                </tfoot>
+              </table>
+            </div>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
